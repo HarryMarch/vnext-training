e0e8939e478e37736aff04ef07a6ef26743834e1
Nguyen Ngoc Cuong <cuongnn@domain.vnext.vn>
2020-04-03 17:46:17 +0700

    wordpress sidebars

--- wp-content/themes/mytheme/archive.php
+++ wp-content/themes/mytheme/archive.php
@@ -3,33 +3,44 @@
 <section class="page-wrap">
 	<div class="container">
 
-		<!-- Show category of these posts -->
-		<h1><?php echo single_cat_title();?></h1>
+		<section class="row">
+			<div class="col-lg-3">
 
-		<!-- Show all posts with template section-archive -->
-	    <?php get_template_part('includes/section','archive'); ?>
+				<?php if(is_active_sidebar('blog-sidebar')):?>
+				<?php dynamic_sidebar('blog-sidebar');?>
+				<?php endif;?>
 
-	    <!-- Show pagination with next, previous button -->
-	    <?php previous_posts_link();?>
-	    <?php next_posts_link();?>
+			</div>	
+			<div class="col-lg-9">		
+
+				<!-- Show category of these posts -->
+				<h1><?php echo single_cat_title();?></h1>
+
+				<!-- Show all posts with template section-archive -->
+			    <?php get_template_part('includes/section','archive'); ?>
+
+			    <!-- Show pagination with next, previous button -->
+			    <?php previous_posts_link();?>
+			    <?php next_posts_link();?>
 
 
-	    
+			    
 
-		<!-- Show pagination with numbers, next, previous button -->
-	    <!--<?php
-	    	/*global $wp_query;
+				<!-- Show pagination with numbers, next, previous button -->
+			    <!--<?php
+			    	/*global $wp_query;
 
-	    	$big = 999999999;
+			    	$big = 999999999;
 
-	    	echo paginate_links( array(
-	    		'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-	    		'format' => '?page=%#%',
-	    		'current' => max(1, get_query_var('paged')),
-	    		'total' => $wp_query->max_num_pages
-	    	) );*/
-	    ?>-->
-
+			    	echo paginate_links( array(
+			    		'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+			    		'format' => '?page=%#%',
+			    		'current' => max(1, get_query_var('paged')),
+			    		'total' => $wp_query->max_num_pages
+			    	) );*/
+			    ?>-->
+			</div>
+		</section>
 	</div>
 </section>
 
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -27,7 +27,7 @@ add_action('wp_enqueue_scripts', 'load_js');
 //Theme Options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
-
+add_theme_support('widgets');
 
 //Menus
 register_nav_menus(
@@ -42,4 +42,31 @@ register_nav_menus(
 //Custom Image Sizes
 add_image_size('blog-large', 800, 400, false);
 add_image_size('blog-small', 300, 200, true);
+
+
+//Register Sidebars
+function my_sidebars()
+{
+    register_sidebar(
+        array(
+            'name' => 'Page Sidebar',
+            'id' => 'page-sidebar',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'blog-sidebar',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>'
+        )
+    );
+}
+add_action('widgets_init', 'my_sidebars');
+
+
+
 ?>
