14b17fe4a5d9144ccc9e7e4cace3228f2d69e499
dattlq1 <dattq@vnext.com.vn>
2020-04-06 17:46:10 +0700

    wordpress template navigation menus

--- themes/mytheme/front-page.php
+++ themes/mytheme/front-page.php
@@ -1,1 +1,16 @@
-This is front page
+<?php get_header(); ?>
+
+<section class="page-wrap"></section>
+<div <div class="container">
+    <h1>
+        <?php
+            the_title();
+        ?> 
+    </h1>    
+    <?php 
+        get_template_part('includes/section', 'content');
+    ?>
+</div>
+
+
+<?php get_footer(); ?>
