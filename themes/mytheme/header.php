c8f5d3a6b97774fcb39e54baf266bac7a1bd7d9b
dattlq1 <dattq@vnext.com.vn>
2020-04-06 11:34:45 +0700

    wordpress enqueueing css and js

---
+++ themes/mytheme/header.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn WordPress</title>
+
+    <?php wp_head(); ?>
+
+</head>
+<body>
+    
