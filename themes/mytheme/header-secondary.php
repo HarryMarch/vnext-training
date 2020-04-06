077f337c47d6d5c493abca79cbb5e37cea12163f
dattlq1 <dattq@vnext.com.vn>
2020-04-06 12:19:19 +0700

    wordpress template parts and page templates

---
+++ themes/mytheme/header-secondary.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn WordPress</title>
+
+    <?php wp_head(); ?>
+
+</head>
+<body>
+    
+This is the secondary header
