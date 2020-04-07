91f086a1f6ea08b19f800e136d46865d0af09a8d
dattlq1 <dattlq@vnext.com.vn>
2020-04-07 17:30:45 +0700

    wordpress blog

--- themes/mytheme/functions.php
+++ themes/mytheme/functions.php
@@ -23,6 +23,7 @@
     add_action('wp_enqueue_scripts', 'load_js');
 
     add_theme_support('menus');
+    add_theme_support('post-thumbnails');
 
     register_nav_menus(
       array(
@@ -32,4 +33,6 @@
       )
     );
 
+    add_image_size('blog-large', 800, 600, false);
+    add_image_size('blog-small', 300, 200, true);
 ?>
--- themes/mytheme/page.php
+++ themes/mytheme/page.php
@@ -6,10 +6,14 @@
             the_title();
         ?> 
     </h1>    
+    <?php
+        if(has_post_thumbnail()):
+    ?>
+        <img src="<?php the_post_thumbnail_url('blog-large'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-3 img-thumbnail">
+    <?php endif; ?>
     <?php 
         get_template_part('includes/section', 'content');
     ?>
 </div>
 
-
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
