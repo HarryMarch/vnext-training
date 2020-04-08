5ee61666ddeacd20ad9b70257fc423afa47480c6
dattlq1 <dattlq@vnext.com.vn>
2020-04-08 16:14:15 +0700

    wordpress search

--- themes/mytheme/front-page.php
+++ themes/mytheme/front-page.php
@@ -3,13 +3,10 @@
 <section class="page-wrap"></section>
 <div <div class="container">
     <h1>
-        <?php
-            the_title();
-        ?> 
+        <?php the_title(); ?> 
     </h1>    
-    <?php 
-        get_template_part('includes/section', 'content');
-    ?>
+    <?php get_template_part('includes/section', 'content'); ?>
+    <?php get_search_form(); ?>
 </div>
 
 
