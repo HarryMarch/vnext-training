e615e4c7d9ad098d11689491e6a99ffb76be53f0
dattlq1 <dattlq@vnext.com.vn>
2020-04-09 15:28:47 +0700

    wordpress custom fields

--- themes/mytheme/functions.php
+++ themes/mytheme/functions.php
@@ -5,6 +5,10 @@
         array(), false, 'all');
         wp_enqueue_style('bootstrap');
 
+        wp_register_style('magnific', get_template_directory_uri().'/css/magnific-popup.css',
+        array(), false, 'all');
+        wp_enqueue_style('magnific');
+
         wp_register_style('main', get_template_directory_uri().'/css/main.css',
         array(), false, 'all');
         wp_enqueue_style('main');
@@ -15,9 +19,18 @@
     function load_js()
     {
         wp_enqueue_script('jquery');
+
         wp_register_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js',
         'jquery', false, true);
         wp_enqueue_script('bootstrap');
+
+        wp_register_script('magnific', get_template_directory_uri().'/js/jquery.magnific-popup.min.js',
+        'jquery', false, true);
+        wp_enqueue_script('magnific');
+
+        wp_register_script('custom', get_template_directory_uri().'/js/custom.js',
+        'jquery', false, true);
+        wp_enqueue_script('custom');
     }
 
     add_action('wp_enqueue_scripts', 'load_js');
@@ -36,7 +49,7 @@
 
     add_image_size('blog-large', 800, 600, false);
     add_image_size('blog-small', 300, 200, true);
-
+    
     function my_sidebars()
     {
         register_sidebar(
@@ -69,7 +82,7 @@
             'public' => true,
             'has_archive' => true,
             'menu_icon' => 'dashicons-images-alt2',
-            'supports' => array('title', 'editor', 'thumbnail'),
+            'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
         );
         register_post_type('cars', $args);
     }
